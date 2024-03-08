7d9577244e94bbb29e71e2ce22230e27dcb14da6
AnaMVB57 <108105538+AnaMVB57@users.noreply.github.com>
2024-03-08 12:12:47 -0500

    Add files via upload

--- productosComoMaps.dart
+++ productosComoMaps.dart
@@ -1,89 +1,103 @@
 // 1.Deberá contar con una clase Producto con los campos: codigo, descripcion, precio y existencia.
 // 2.Convertir cada objeto Producto en un mapa (Map) y mostrar el resultado
-//como un listado de Maps similar al listado anterior.
+//como un listado de Maps.
 
 import 'dart:io';
 
 void main() {
-  // Producto prueba1 = Producto(
-  //     codigo: 1, descripcion: 'Guayaba 1kg', precio: 50000, existencia: 10);
-
-  // Map prueba2 = {
-  //   'codigo': 1,
-  //   'descripcion': 'Limón 1kg',
-  //   'precio': 30000,
-  //   'existencia': 15
-  // };
-
-  // print(prueba1.toString());
-  // print(prueba2);
-
   menu();
 }
 
 class Producto {
   // Declaración de variables
-  int codigo;
+  String codigo;
   String descripcion;
   double precio;
   int existencia;
 
-  //Método constructor del producto, abajo se inicializan las variables e
-  // indicamos el campo codigo como requerido.
+  //Método constructor del producto, abajo se inicializan las variables
   Producto(
-      {required this.codigo,
+      {required this.codigo, // indicamos el campo codigo como requerido.
       this.descripcion = '',
       this.precio = 0,
       this.existencia = 0}) {}
+
+  //Convierte un objeto en un mapa
+  Map convertirMapa() {
+    return {
+      'Código': codigo,
+      'Descripción': descripcion,
+      'Precio': precio,
+      'Existencia': existencia
+    };
+  }
 
   String toString() {
     return 'Producto: [Código: $codigo, Descripción: $descripcion, Precio: $precio, Existencia: $precio]';
   }
 }
 
+//Método que muestra las opciones del menú y ejecuta las respectivas funciones
 void menu() {
-  dynamic contador = 0;
-  Map listado = Map();
-  mostrarMenu();
-  int opc = leerInt("Ingrese una opción: ");
-  while (opc != 4) {
+  List listado = [];
+  int opc;
+  do {
+    mostrarMenu();
+    opc = leerInt("Ingrese una opción: \n____________________________");
     switch (opc) {
       case 1:
-        // listado.addEntries();
+        Producto producto = crearProducto();
+        listado.add(producto.convertirMapa());
         break;
       case 2:
-        // mostrarListado();
+        mostrarListado(listado);
         break;
       case 3:
-        // eliminarProducto();
+        print("Gracias. Hasta luego.\n____________________________");
         break;
-      case 4:
-        print("Gracias. Hasta luego.");
-        break;
-      default:
-        print("Ingrese una opción válida, por favor.");
     }
-  }
+  } while (opc != 3);
 }
 
+//Método que muestra las opciones del menú
 mostrarMenu() {
-  Map menu = {
-    '1': 'Ingresar producto',
-    '2': 'Mostrar listado',
-    '3': 'Eliminar producto(s) por id',
-    '4': 'Salir'
-  };
+  print("________¡Bienvenido!________");
+  Map menu = {'1': 'Ingresar producto', '2': 'Mostrar listado', '3': 'Salir'};
 
   menu.forEach((key, value) {
-    print(value);
+    print(key + ". " + value);
   });
 }
 
-void crearProducto() {
-  print("Ingrese los datos del producto: ");
+//Método para mostrar la lista de productos
+mostrarListado(List listado) {
+  int contador = 1;
+  print("Productos existentes en la lista: ");
+  listado.forEach((element) {
+    print("$contador. $element");
+    contador++;
+  });
+  print("____________________________");
 }
 
-//Método que lee datos de tipo int
+//Método para crear productos
+Producto crearProducto() {
+  print("Ingrese los datos del producto: ");
+  print("Código del producto");
+  String cod = stdin.readLineSync().toString();
+  print("Descripción del producto: ");
+  String desc = stdin.readLineSync().toString();
+  double prec = leerInt("Precio del producto: ").toDouble();
+  int exis = leerInt("Existencia del producto: ");
+
+  Producto producto =
+      Producto(codigo: cod, descripcion: desc, precio: prec, existencia: exis);
+
+  print("*** Producto agregado al listado. ***");
+  return producto;
+}
+
+//Método que lee entradas de datos, los convierte y retorna datos de tipo int
 int leerInt(String mensaje) {
   print(mensaje);
   String input = stdin.readLineSync().toString();
